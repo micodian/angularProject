57e4482943b8dc07062ebfa5c1d87e9838492a8a
micodian <micodian83@gmail.com>
2020-09-03 19:04:18 -0500

    practised with typescript code

--- ts-code/newCode.js
+++ ts-code/newCode.js
@@ -3,3 +3,22 @@ function log(msg) {
 }
 var msg = 'Hello World';
 log(msg);
+var a = 10;
+var b = true;
+var c = 'Hello';
+var d = 23.4;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Blue"] = 1] = "Blue";
+    Color[Color["Green"] = 2] = "Green";
+})(Color || (Color = {}));
+;
+var insideColor = Color.Red;
+var message;
+message = 'abc';
+var storedVariable = message.endsWith('c');
+//storing function values using arrow functions
+var value = function (param) {
+    console.log('How are you');
+};
