64e59fb6887d75858fb8292dd4556027b299818e
micodian <micodian83@gmail.com>
2020-09-04 17:57:12 -0500

    module updated

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CourseComponent,
     CoursesComponent
+    
+   
   ],
   imports: [
     BrowserModule,
