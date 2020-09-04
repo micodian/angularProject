bfec03e5340ee1a8e2195ef60205bc4d80d2a936
micodian <micodian83@gmail.com>
2020-09-04 17:58:09 -0500

    worked on using directives *ngFor to print out list

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -4,12 +4,22 @@ import{ Component } from '@angular/core';
 @Component({
     //add properties of this component
     //anywhere there is an element for courses, angular would render the template
+    //using string interpolation
     selector: 'courses',
-    template: '<h2>{{ title }}</h2>'
+    template: `
+    <h2>{{ title }}</h2>
+    <ul>
+        <li *ngFor = "let course of courses">
+            {{ course }}
+        </li>
+    </ul>
+
+    `
 
 
 })
 export class CoursesComponent{
     //adding a field
     title = "List of courses";
+    courses =["course1","course2","course3"];
 }
