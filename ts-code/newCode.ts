5395e44c1e3feac43d41eb8326fff1adab3a7ae7
micodian <micodian83@gmail.com>
2020-09-04 16:01:25 -0500

    tested soft reset with git

--- ts-code/newCode.ts
+++ ts-code/newCode.ts
@@ -5,3 +5,20 @@ function log(msg){
 var msg = 'Hello World';
 
 log(msg);
+
+let a: number = 10;
+let b: boolean = true;
+let c: string = 'Hello';
+let d: any = 23.4;
+
+enum Color {Red, Blue, Green};
+let insideColor = Color.Red;
+
+let message;
+message = 'abc';
+let storedVariable =(<string>message).endsWith('c');
+
+//storing function values using arrow functions
+let value = (param) =>{
+    console.log('How are you');
+}
